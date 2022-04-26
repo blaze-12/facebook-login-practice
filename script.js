763fb06cc8f659ba98452d735f5125bb34e40f0d
blazess <damhat2007@gmail.com>
2022-04-25 19:16:29 -0700

    Add files via upload
    added some files

---
+++ script.js
@@ -0,0 +1,9 @@
+let test = document.querySelector("h1")
+
+test.onclick = function() {
+    test.innerText = "Login to facebook";
+}
+
+// test.onclick = function() {
+//     test.innerTest = "Sign up for Facebook"
+// }
